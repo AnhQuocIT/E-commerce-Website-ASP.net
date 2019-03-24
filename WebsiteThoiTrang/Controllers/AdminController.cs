6335ae30f87da534999ef36f53302428d12e60f3
AnhQuocIT <anhquocpro0206@gmail.com>
2019-03-24 14:30:36 +0700

    Update Admin Controllers/AdminController.cs

---
+++ WebsiteThoiTrang/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteThoiTrang.Models;
+using PagedList;
+using PagedList.Mvc;
+using System.IO;
+using System.Data.Entity;
+
+namespace WebsiteThoiTrang.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        dbWebDataContext data = new dbWebDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TenAdmin()
+        {
+            return PartialView();
+        }
+
+        //Tiếp theo ở đây
+	
+    }
+}
